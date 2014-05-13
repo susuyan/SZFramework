af81704b5816bfa70f84c7be8930d794ec04466e
zhengtao <tao.zheng@yuelian.com>
2014-05-13 22:27:05 +0800

    SZNotificationView

---
+++ SZFramework/SZNotificationView.h
@@ -0,0 +1,82 @@
+//
+//  SZNotificationView.h
+//  SZFramework
+//
+//  Created by Sid_Zheng on 14-5-13.
+//  Copyright (c) 2014年 3MeoW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+static CGFloat const kSZNotificationViewHeight = 50.0f;
+static CGFloat const kSZNotificationViewSymbolViewSidelength = 44.0f;
+static CGFloat const kSZNotificationViewDefaultShowDuration = 2.0;
+
+typedef NS_ENUM(NSInteger, SZNotificationViewStyle) {
+    SZNotificationViewStyleSuccess = 0,
+    SZNotificationViewStyleError,
+    SZNotificationViewStyleText,
+    SZNotificationViewStyleImage
+};
+
+typedef NS_ENUM(NSInteger, SZNotificationViewPosition) {
+    SZNotificationViewPositionTop = 0,
+    SZNotificationViewPositionBottom
+};
+
+typedef void (^SZNotificationViewTapHandler)();
+
+@interface SZNotificationView : UIView
+
+#pragma mark + quick presentation
+
++ (void)showInViewController:(UIViewController*)viewController
+                       style:(SZNotificationViewStyle)style
+                     message:(NSString*)message;
+
++ (void)showInViewController:(UIViewController*)viewController
+                   tintColor:(UIColor*)tintColor
+                       image:(UIImage*)image
+                     message:(NSString*)message
+                    duration:(NSTimeInterval)duration;
+
++ (void)showInViewController:(UIViewController*)viewController
+                   tintColor:(UIColor*)tintColor
+                        font:(UIFont*)font
+               textAlignment:(NSTextAlignment)textAlignment
+                       image:(UIImage*)image
+                     message:(NSString*)message
+                    duration:(NSTimeInterval)duration;
+
+#pragma mark + creators
+
++ (SZNotificationView *)notificationViewWithParentViewController:(UIViewController*)viewController
+                                                       tintColor:(UIColor*)tintColor
+                                                           image:(UIImage*)image
+                                                         message:(NSString*)message;
+
+#pragma mark - initialization
+
+- (instancetype)initWithParentViewController:(UIViewController*)viewController;
+
+#pragma mark - presentation
+
+- (void)setVisible:(BOOL)showing animated:(BOOL)animated completion:(void (^)())completion;
+- (void)dismissWithStyle:(SZNotificationViewStyle)style message:(NSString*)message duration:(NSTimeInterval)duration animated:(BOOL)animated;
+@property (readonly, nonatomic, getter = isShowing) BOOL visible;
+
+#pragma mark - visible properties
+
+/**
+ The image property should be used for setting the image displayed in imageView
+ Only the alpha value will be used and then be tinted to a 'legible' color
+ */
+@property (nonatomic, strong) UIImage* image;
+
+@property (nonatomic, strong) UIColor* tintColor;
+
+@property (nonatomic, getter = isShowingActivity) BOOL showingActivity;
+
+@property (nonatomic, copy) SZNotificationViewTapHandler tapHandler;
+
+@end
