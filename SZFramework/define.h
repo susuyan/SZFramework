f28ca0af5b354ca3da4f83e2fb38c8171acc871d
Sid_Zheng <iossid@icloud.com>
2014-12-17 16:20:36 +0800

    debug

--- SZFramework/define.h
+++ SZFramework/define.h
@@ -14,8 +14,8 @@
 // device
 #define ScreenSize ([UIScreen mainScreen].bounds.size)
 #define is_4inch (ScreenSize.height > 480)
-#define is_iOS7 (floor(NSFoundationVersionNumber) >= NSFoundationVersionNumber_iOS_7_0)
 #define is_iOS8 (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1)
+#define is_iOS7 (!is_iOS8)
 
 // color
 #define RGB(r, g, b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
